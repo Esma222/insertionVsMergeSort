b55b14f55a69cef8cb60cab603d615c8e7748c9e
Esma Beydili <106523107+Esma222@users.noreply.github.com>
2023-07-25 02:10:47 +0300

    Update test.java

--- insertionVsMergeSort/test.java
+++ insertionVsMergeSort/test.java
@@ -7,7 +7,7 @@ public class test {
 
 	public static void main(String[] args) throws FileNotFoundException  {
 		// TODO Auto-generated method stub
-		//part1
+		// part1
 		System.out.println("**ins.");
 		int[] arrI = {4,3,2,10,12,1,5,6};
 		int[] arrI2 = insertion_sort.insertion_sort(arrI);
